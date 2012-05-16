61519c664d813d894147374d3f3659a863b8acad
kook <kook@kook-System-Name.(none)>
2012-05-16 23:15:41 +0200

    working really nicely

--- src/sketch.ino
+++ src/sketch.ino
@@ -2,16 +2,26 @@
 
 
 
+byte bri = 0;
+const byte levels = 10;
+const byte width = 4;//of display in bytes
+byte leds[levels][width][16];
 
-
-
-byte leds[8][16];
 void led(int x, int y, int b)
 {
-    if (b >= 1)
-        leds[x/8][y] &=~ (1<<(x%8));
-    else
-    	leds[x/8][y] |=  (1<<(x%8));
+    if(x<0)return;
+    if(x>=width*8)return;
+    if(y<0)return;
+    if(y>=16)return;
+    
+    int l;
+    for(l=0;l<levels;l++)
+    {
+	if (b > l)
+            leds[l][x/8][y] &=~ (1<<(x%8));
+    	else
+    	    leds[l][x/8][y] |=  (1<<(x%8));
+    }
 }
 
 
@@ -24,13 +34,13 @@ typedef struct
 } particle;
 
 
-const int PS = 3*5;
+const int PS = 6;
 particle ps[PS];
 
 
 void bounce(int i)
 {
-        if(ps[i].x>48)
+        if(ps[i].x>width*8)
 		ps[i].a=-ps[i].a;
         if(ps[i].x<1)
 		ps[i].a=-ps[i].a;
@@ -50,28 +60,35 @@ void boing()
     for (i=0;i<PS;i++)
     {
     	led(ps[i].x,ps[i].y,0);
+
 	bounce(i);
         ps[i].x+=ps[i].a;
 	ps[i].y+=ps[i].b;
+
         led(ps[i].x,ps[i].y,100);
     }
 }
 
 
+void cls(byte b)
+{
+    int i,j;
+    for(i = 0; i < width*8; i++)
+    	for(j = 0; j < 16; j++)
+	    led(i,j,b);
+}
+
 
 void setupleds()
 {
-    int i,j;
-    for(i = 0; i < 6*8; i++)
-    	for(j = 0; j < 16; j++)
-	    led(i,j, 0);
-
+    cls(bri);
+    int i;
     for (i=0;i<PS;i++)
     {
         ps[i].a=(random(2000)-1000)/10000.0;
         ps[i].b=(random(2000)-1000)/10000.0;
-        ps[i].x=random(36)+10;
-        ps[i].y=random(11)+1;
+        ps[i].x=random(width*8);
+        ps[i].y=random(16);
     }
 }
 
@@ -100,7 +117,7 @@ void setup()
     pinMode(OD,OUTPUT);
     digitalWrite(OD, 1);
     pinMode(RND, INPUT);
-    randomSeed(analogRead(RND));
+    randomSeed(10);
     SPI.setClockDivider(2);
     SPI.setBitOrder(LSBFIRST);
     SPI.begin();
@@ -108,25 +125,24 @@ void setup()
     Serial.begin(57600);
 }
 
-char brightness;
+
+int level=1;
 byte row,frame=1;
 void loop()
 {
     int i,j;
-
-    SPI.transfer(255);
-    int dead = 0;
-
-    for(i = 0; i < 6; i++)
+    int brightness = level;
+    for(i = 0; i < width; i++)
     {
-	PORTD=0b00000000;
-        for(j = (!dead); j < 4; j++)
+	if(brightness-->0)PORTD=0b00000000;
+        for(j = 0; j < 4; j++)
         {
-	    dead++;
+	    if(brightness==0)PORTD=0b01000000;
 	    int y = row+j*4;
+	    if(brightness==1)PORTD=0b01000000;
+	    SPI.transfer(leds[level][i][y]);
 	    PORTD=0b01000000;
 
-	    SPI.transfer(leds[i][y]);
 
 	}
     }
@@ -134,19 +150,32 @@ void loop()
     digitalWrite(ST, 0);
     PORTC = row<<4;
     digitalWrite(ST, 1);
-    brightness = 6*((analogRead(RND)-100)/5);
-    
 
-    if (++frame >15)
+    if (++frame >8)
     {
 	boing();
 	frame = 0;
-	Serial.println((int)brightness);
+	if(Serial.available())
+	{
+	    switch(Serial.read()){
+		case '+':
+		    bri++;
+		    break;
+		case '-':
+		    bri--;
+		    break;
+	    }
+	    cls(bri);
+	    Serial.println(bri);
+	}
     }
     
     if(4==++row)
     {
 	row = 0;
+	level++;
+	if(level == levels)
+	    level = 0;
     }
     
 }
